35f7aac42c004b6f442757406c9018e2944ae643
Farzana <akbarifarzana77@gmail.com>
2026-01-13 17:02:38 +0400

    Update README

--- week_02/pizza_order.py
+++ week_02/pizza_order.py
@@ -1,31 +1,26 @@
 print("Welcome to Pizza Deliveries!")
 
 bill = 0
-
 # Ask for pizza size
 size = input("What size pizza do you want? S, M, or L: ")
-
 if size == "S":
     bill = 15
 elif size == "M":
     bill = 20
 elif size == "L":
     bill = 25
-
-# Ask for pepperoni
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ") # Ask for pepperoni
 if pepperoni == "Y":
     if size == "S":
         bill += 1
     else:
         bill += 2
 
-# Ask for extra cheese
+              # Ask for extra cheese
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
 if extra_cheese == "Y":
     bill += 1
 
-# Final result
+# result
 print(f"Your final bill is: ${bill}.")
