09757d5ab0f11c89fdced6771a8c74907018c728
Farzana <akbarifarzana77@gmail.com>
2026-04-05 11:01:16 +0400

    Update hangman_step.py

--- week-08/hangman_step.py
+++ week-08/hangman_step.py
@@ -1,17 +1,124 @@
-chosen_word = "fox"
-guess = "x"
-# TODO 4: create placeholder
-placeholder = ""
-for letter in chosen_word:
-    placeholder += "_"
+import random
 
-print("Placeholder:", placeholder)
-# TODO 5: create display
-display = ""
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
+word_list = ["python", "developer", "hangman", "challenge", "programming"]
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+display = ["_"] * word_length
+guessed_letters = []
+lives = 6
+
+print(" Welcome to Hangman!")
+print("Word:", " ".join(display))
+
+stages = [
+r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''',
+r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+'''
+]
+
+game_over = False
+
+while not game_over:
+    guess = input("\nGuess a letter: ").lower()
+
+    # Validate input
+    if not guess.isalpha() or len(guess) != 1:
+        print(" Please enter a single letter.")
+        continue
+
+    # Already guessed
+    if guess in guessed_letters:
+        print(" You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    # Correct guess
+    if guess in chosen_word:
+        for position in range(word_length):
+            if chosen_word[position] == guess:
+                display[position] = guess
+        print(" Correct!")
     else:
-        display += "_"
+        lives -= 1
+        print(" Wrong!")
 
-print("Display:", display)
+    # Show hangman stage
+    print(stages[6 - lives])
+
+    # Show progress
+    print("Word:", " ".join(display))
+    print("Guessed letters:", ", ".join(guessed_letters))
+
+    # Win condition
+    if "_" not in display:
+        print("\n🎉 Congratulations! You WIN!")
+        game_over = True
+
+    # Lose condition
+    if lives == 0:
+        print("\n💀 Game Over! You LOSE!")
+        print("The word was:", chosen_word)
+        game_over = True
