40246bbd1690ce6dab9206dc8632b2d158090996
Farzana <akbarifarzana77@gmail.com>
2026-04-10 12:29:27 +0400

    initial commit

---
+++ week-10/Secret Auction Program.py
@@ -0,0 +1,35 @@
+from art import logo
+
+print(logo)
+print("Welcome to the secret auction program.")
+
+bids = {}
+continue_bidding = True
+
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+
+    if should_continue == "yes":
+        print("\n" * 20)  # clears the screen (simple way)
+    else:
+        continue_bidding = False
+
+
+def find_highest_bidder(bidding_dictionary):
+    highest_bid = 0
+    winner = ""
+
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+find_highest_bidder(bids)
